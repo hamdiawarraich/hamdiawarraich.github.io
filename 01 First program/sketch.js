338ab15d0692c5184a1374e9d28bab617433be0d
hamdiawarraich <hamdiaasad06@gmail.com>
2025-09-11 11:50:25 -0600

    upload

---
+++ 01 First program/sketch.js
@@ -0,0 +1,14 @@
+// First program
+// Hamdia Warraich 
+// 09-09-2025
+
+function setup() { //rune ONCE at the start 
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() { //runs OVER and OVER 
+                  //targeting 60 frames per second 
+  //background(220);
+  fill(255,0,0);
+  circle(mouseX, mouseY, 30);
+}
