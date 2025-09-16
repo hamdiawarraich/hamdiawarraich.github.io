3d137e9808e742f61a18bb82d04550bb1f3e9834
hamdiawarraich <hamdiaasad06@gmail.com>
2025-09-16 14:10:19 -0600

    assignment progress

--- 04 Drawing with shapes/sketch.js
+++ 04 Drawing with shapes/sketch.js
@@ -5,12 +5,29 @@
 
 function setup() {
   createCanvas(700,700);
-  rectMode(CENTER)
-  x= width/2;
+  background(225);
+  noStroke();
 }
 
 function draw() {
   background(220);
-  
-
 }
+
+//Alien Head 
+fill(144, 234,144);
+ellipse(width/2, height/2, 100, 100);
+
+//Bottom Body 
+fill(144, 234,144);
+rectMode(CENTER);
+rect(width/2, height/2, 100, 50);
+
+//eyes
+stroke(0);
+strokeWeight(10);
+
+
+//Mouth
+stroke(0);
+strokeWeight(3);
+line();
