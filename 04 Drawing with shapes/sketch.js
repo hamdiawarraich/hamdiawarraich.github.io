6bbe8a3cd947cd546fd797825dad0bbdb69f3dae
Hamdia Warraich <hamdiaasad06@gmail.com>
2025-09-18 16:10:57 -0600

    edited.

--- 04 Drawing with shapes/sketch.js
+++ 04 Drawing with shapes/sketch.js
@@ -3,31 +3,40 @@
 // September 15, 2025 
 //
 
+//Declaring global variables
+
+//let boxX = 200, boxY = 100;
+let tsize = 15;
+let refX, refY;
+
 function setup() {
-  createCanvas(700,700);
-  background(225);
-  noStroke();
+  createCanvas(500,500);
 }
 
 function draw() {
   background(220);
+
+  //drawBox();
+  drawCharacter();
+  fill(0); //black
+  textSize(tsize); 
+  text(mouseX + "," + mouseY + " " + mouseButton, mouseX, mouseY);
+  noStroke();
 }
 
 //Alien Head 
-fill(144, 234,144);
-ellipse(width/2, height/2, 100, 100);
+fill(144, 234,144); //light green color
+circle(width/2, height/2 - 50, 100);
 
 //Bottom Body 
 fill(144, 234,144);
 rectMode(CENTER);
 rect(width/2, height/2, 100, 50);
 
-//eyes
-stroke(0);
-strokeWeight(10);
 
-
-//Mouth
-stroke(0);
-strokeWeight(3);
-line();
+fill(0); //black
+//Eyes 
+ellipse(width/2 - 20, height/2 - 60, 20, 30);
+ellipse(width/2 + 20, height/2 - 60, 20, 30);
+//mouth 
+rect(width/2, height/2 - 30, 50, 10, 20);
